15f63054583ccb1b26679387751cdbfce166cbf9
l-1cm-l <l-----1.5cm----l@i.softbank.jp>
2021-09-04 15:35:55 +0900

    [processing]

---
+++ Method_test.java
@@ -0,0 +1,11 @@
+public class Method_test {
+    public static void main(String[] args){
+    int calc = sum(100,50);
+    System.out.println("合計は"+calc);    
+}
+
+static int sum(int num1,int num2){
+    int sum = num1 + num2;
+    return sum;
+    }
+}

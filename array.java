6fc9f56608b6d1aa5135f4df753bad4f46c763f6
l-1cm-l <l-----1.5cm----l@i.softbank.jp>
2021-09-04 10:38:07 +0900

    [practice]

--- array.java
+++ array.java
@@ -7,6 +7,17 @@ public class array {
         System.out.println(arr[0] );
         System.out.println(arr[1] );
         System.out.println(arr[2] );
+
+        String[][] arr2;
+        arr2 = new String[2][2];
+
+        arr2[0][0] = "山田";
+        arr2[0][1] = "根本";
+        arr2[1][0] = "岸田";
+
+        System.out.println(arr2[0][0]);
+        System.out.println(arr2[0][1]);
+        System.out.println(arr2[1][0]);
     }
     
 }

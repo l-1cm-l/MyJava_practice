6972386e5cae7fa91dbfe645af5eb8ad931a02cc
l-1cm-l <l-----1.5cm----l@i.softbank.jp>
2021-09-04 10:21:43 +0900

    first commit

---
+++ array.java
@@ -0,0 +1,10 @@
+public class array {
+    public static void main(String args[]){
+        String[] arr = {"太郎","光","浩二"};
+
+        System.out.println(arr[0] );
+        System.out.println(arr[1] );
+        System.out.println(arr[2] );
+    }
+    
+}
---
+++ hensuu.java
@@ -0,0 +1,11 @@
+public class hensuu {
+    public static void main(String args[]){
+        int var1 = 1;
+        int var2 = 10;
+        boolean var_bool;
+
+        var_bool = (var1 < var2);
+
+        System.out.println(var_bool);
+    }
+}

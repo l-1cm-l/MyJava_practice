51f9a5b95c8c0add52967bb0bc3ef0eae8f21554
l-1cm-l <l-----1.5cm----l@i.softbank.jp>
2021-09-04 10:29:13 +0900

    [practice]

--- array.java
+++ array.java
@@ -2,6 +2,8 @@ public class array {
     public static void main(String args[]){
         String[] arr = {"太郎","光","浩二"};
 
+        arr[1] = "闇";
+
         System.out.println(arr[0] );
         System.out.println(arr[1] );
         System.out.println(arr[2] );

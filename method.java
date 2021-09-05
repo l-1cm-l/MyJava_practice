e66fb25e3f54e23747e2711e2b0ee8fb69f2e5ce
l-1cm-l <l-----1.5cm----l@i.softbank.jp>
2021-09-05 10:47:29 +0900

    [practice]

--- method.java
+++ method.java
@@ -14,6 +14,6 @@ static int sum(int i,int j){
 class Hiretu{
     public static void main(String args[]){
         int arr[] = {1,2,3,4,5};
-        System.out.println(arr[2]);
+        System.out.println(arr[3]);
     }
 }

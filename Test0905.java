0bdc74fa5d2e88a1747ea2d7847823b2c034c3f2
l-1cm-l <l-----1.5cm----l@i.softbank.jp>
2021-09-05 10:30:05 +0900

    [practice]

--- Test0905.java
+++ Test0905.java
@@ -16,3 +16,15 @@ public class Test0905 {
    }
  }
 }
+
+class Testarray{
+    public static void main(String args[]){
+        int arr[] = {2,4,6,8,10};
+        int sum = 0;
+
+        for(int i =0;i < 5;i++){
+            sum += arr[i];
+        }
+        System.out.println(sum);
+    }
+}

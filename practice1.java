c9e70620d012ea698ea6f5b9b39a770ce7196b5d
l-1cm-l <l-----1.5cm----l@i.softbank.jp>
2021-09-10 16:11:22 +0900

    [practice]

---
+++ practice1.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+
+public class practice1 {
+    public static void main(String[] args){
+        int age = 20;
+        
+        if(age>20){
+            System.out.println("成人");
+        }else if(age == 20){
+            System.out.println("ハタチ");
+        }else{
+            System.out.println("未成年");
+        }
+        System.out.println(age);
+    }
+}

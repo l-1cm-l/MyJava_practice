d58199661fe50e4166071e614865b1661b7ca16a
l-1cm-l <l-----1.5cm----l@i.softbank.jp>
2021-09-11 10:10:05 +0900

    [practice]

---
+++ method0911.java
@@ -0,0 +1,11 @@
+public class method0911 {
+    public static void main(String[] args){
+       int result = sum(1,2);
+       System.out.println(result);
+    }
+    static int sum(int num1,int num2){
+        int calc = num1+num2;
+        return calc;
+    }
+}
+
